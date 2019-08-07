6ef2ce63d235998db0d469f097b6469c4d9d3d39
MillerMC96 <pywang@ucdavis.edu>
2019-08-07 16:53:36 -0700

    semi working

--- src/plot.py
+++ src/plot.py
@@ -4,4 +4,13 @@ import matplotlib.pyplot as plt
 from matplotlib import colors
 from matplotlib.ticker import PercentFormatter
 
+fp = open('../txt/LJ-SR-bottleneck-bottleneck', 'r')
+while fp:
+    line = fp.readline()
+    if line:
+        x, y = line.split()
+        print(x, y)
+    else:
+        break
+
 

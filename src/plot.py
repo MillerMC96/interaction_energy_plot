2f31a0555a8d11d78d68d393efebcc0dfd1c042e
MillerMC96 <pywang@ucdavis.edu>
2019-08-08 22:24:22 -0700

    horizontal

--- src/plot.py
+++ src/plot.py
@@ -5,12 +5,22 @@ from matplotlib import colors
 from matplotlib.ticker import PercentFormatter
 
 fp = open('../txt/LJ-SR-bottleneck-bottleneck', 'r')
+
+time = []
+energy = []
+
+n_bins = 50
+
 while fp:
     line = fp.readline()
     if line:
         x, y = line.split()
-        print(x, y)
+        time.append(float(x))
+        energy.append(float(y))
     else:
         break
-
-
+plt.hist(energy, bins=n_bins, orientation="horizontal")
+plt.ylabel("energy [kJ/mol]")
+plt.xlabel("frequency")
+plt.title("energy distribution for bottleneck")
+plt.show()

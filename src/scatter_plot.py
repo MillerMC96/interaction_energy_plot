9149ce0b6bc85511a6c377628e08c63f5abe4368
MillerMC96 <pywang@ucdavis.edu>
2019-08-14 15:25:26 -0700

    added pull indicators

--- src/scatter_plot.py
+++ src/scatter_plot.py
@@ -7,11 +7,11 @@ from matplotlib.ticker import PercentFormatter
 
 fp = open(sys.argv[1], 'r')
 
+# x and y axes
 time = []
 energy = []
 
-n_bins = 50
-
+# reading input
 while fp:
     line = fp.readline()
     if line:
@@ -20,7 +20,43 @@ while fp:
         energy.append(float(y))
     else:
         break
+
 plt.plot(time, energy, linewidth=1)
+
+#pull indicators
+
+#first pull
+plt.axvline(0)
+plt.text(0.1, -100, 'first pull', rotation=90)
+
+#second pull
+plt.axvline(60)
+plt.text(60.1, -100, 'second pull', rotation=90)
+
+#third pull
+plt.axvline(90)
+plt.text(90.1, -100, 'third pull', rotation=90)
+
+#fourth pull
+plt.axvline(120)
+plt.text(120.1, -100, 'fourth pull', rotation=90)
+
+#fifth pull
+plt.axvline(150)
+plt.text(150.1, -100, 'fifth pull', rotation=90)
+
+#sixth pull
+plt.axvline(180)
+plt.text(180.1, -100, 'sixth pull', rotation=90)
+
+#seventh pull
+plt.axvline(210)
+plt.text(210.1, -100, 'seventh pull', rotation=90)
+
+#eighth pull
+plt.axvline(270)
+plt.text(270.1, -200, 'eighth pull', rotation=90)
+
 plt.ylabel("energy [kJ/mol]")
 plt.xlabel("time [ps]")
 plt.title(sys.argv[1])

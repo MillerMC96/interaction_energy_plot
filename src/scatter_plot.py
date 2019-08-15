bd914e6f6a4e97bde0cde7a0fd4a7f5124ac2ce5
MillerMC96 <pywang@ucdavis.edu>
2019-08-15 13:53:05 -0700

    sync

--- src/scatter_plot.py
+++ src/scatter_plot.py
@@ -35,38 +35,39 @@ plt.ylim([bottom, top])
 
 transparency = 0.7
 text_spacing = 20
+offset_from_indicator = 0.2
 
 #first pull
 plt.axvline(0, alpha=transparency) 
-plt.text(0.1, bottom + text_spacing, 'first pull', rotation=90)
+plt.text(0 + offset_from_indicator, bottom + text_spacing, 'first pull', rotation=90)
 
 #second pull
 plt.axvline(60, alpha=transparency)
-plt.text(60.1, bottom + text_spacing, 'second pull', rotation=90)
+plt.text(60 + offset_from_indicator, bottom + text_spacing, 'second pull', rotation=90)
 
 #third pull
 plt.axvline(90, alpha=transparency)
-plt.text(90.1, bottom + text_spacing, 'third pull', rotation=90)
+plt.text(90 + offset_from_indicator, bottom + text_spacing, 'third pull', rotation=90)
 
 #fourth pull
 plt.axvline(120, alpha=transparency)
-plt.text(120.1, bottom + text_spacing, 'fourth pull', rotation=90)
+plt.text(120 + offset_from_indicator, bottom + text_spacing, 'fourth pull', rotation=90)
 
 #fifth pull
 plt.axvline(150, alpha=transparency)
-plt.text(150.1, bottom + text_spacing, 'fifth pull', rotation=90)
+plt.text(150 + offset_from_indicator, bottom + text_spacing, 'fifth pull', rotation=90)
 
 #sixth pull
 plt.axvline(180, alpha=transparency)
-plt.text(180.1, bottom + text_spacing, 'sixth pull', rotation=90)
+plt.text(180 + offset_from_indicator, bottom + text_spacing, 'sixth pull', rotation=90)
 
 #seventh pull
 plt.axvline(210, alpha=transparency)
-plt.text(210.1, bottom + text_spacing, 'seventh pull', rotation=90)
+plt.text(210 + offset_from_indicator, bottom + text_spacing, 'seventh pull', rotation=90)
 
 #eighth pull
 plt.axvline(270, alpha=transparency)
-plt.text(270.1, bottom + text_spacing, 'eighth pull', rotation=90)
+plt.text(270 + offset_from_indicator, bottom + text_spacing, 'eighth pull', rotation=90)
 
 plt.ylabel("energy [kJ/mol]")
 plt.xlabel("time [ps]")
